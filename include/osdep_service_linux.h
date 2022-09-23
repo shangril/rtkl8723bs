a6cc7252c80a139a5aab9a5a12d30dbcdc6c99da
shangril <shangril@users.noreply.github.com>
2022-09-24 01:55:08 +0200

    Update osdep_service_linux.h

--- include/osdep_service_linux.h
+++ include/osdep_service_linux.h
@@ -271,7 +271,7 @@ __inline static void _init_timer(_timer *ptimer,_nic_hdl nic_hdl,void *pfunc,voi
 	//setup_timer(ptimer, pfunc,(u32)cntx);	
 	ptimer->function = pfunc;
 	ptimer = * (unsigned long)cntx;
-	init_timer(ptimer);
+	_init_timer(ptimer);
 }
 
 __inline static void _set_timer(_timer *ptimer,u32 delay_time)
