3b7eb76f45ffb0193a1c7610fb593e9d0b4488be
shangril <shangril@users.noreply.github.com>
2022-09-24 21:22:56 +0200

    Update osdep_service_linux.h

--- include/osdep_service_linux.h
+++ include/osdep_service_linux.h
@@ -154,7 +154,7 @@
 	typedef void*	thread_context;
 	typedef long retvalue; 
 
-	#define thread_exit(retvalue) kthread_complete_and_exit(NULL, retvalue)
+	//#define thread_exit(retvalue) kthread_complete_and_exit(NULL, retvalue)
 
 	typedef void timer_hdl_return;
 	typedef void* timer_hdl_context;
