1b1aeaa448bde2ed67e943f8adfa3679e8a1fc6d
shangril <shangril@users.noreply.github.com>
2022-09-24 22:53:59 +0200

    Update rtw_mlme_ext.c

--- core/rtw_mlme_ext.c
+++ core/rtw_mlme_ext.c
@@ -1194,7 +1194,7 @@ void mgt_dispatcher(_adapter *padapter, union recv_frame *precv_frame)
 				ptable->func = &OnAuth;
 			else
 				ptable->func = &OnAuthClient;
-			continue; 
+			__attribute__((fallthrough)) 
 		case WIFI_ASSOCREQ:
 		case WIFI_REASSOCREQ:
 			_mgt_dispatcher(padapter, ptable, precv_frame);	
