0fe14b4052b18a218486e8018bfac3181a61d46b
shangril <shangril@users.noreply.github.com>
2022-09-25 21:20:02 +0200

    Update ioctl_cfg80211.c

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -2406,8 +2406,9 @@ if (padapter->registrypriv.mp_mode == 1)
 	#ifdef CONFIG_P2P
 	if( pwdinfo->driver_interface == DRIVER_CFG80211 )
 	{
-		if(ssids->ssid != NULL
-			&& _rtw_memcmp(ssids->ssid, "DIRECT-", 7)
+		if(//ssids->ssid != NULL
+			//&& 
+			_rtw_memcmp(ssids->ssid, "DIRECT-", 7)
 			&& rtw_get_p2p_ie((u8 *)request->ie, request->ie_len, NULL, NULL)
 		)
 		{
