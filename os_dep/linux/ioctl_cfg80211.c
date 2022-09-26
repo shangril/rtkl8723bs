5be516f246c93e332b80b168491af55fa61be37c
shangril <shangril@users.noreply.github.com>
2022-09-26 10:15:19 +0200

    Update ioctl_cfg80211.c

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -805,7 +805,7 @@ check_bss:
 			#if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 39) || defined(COMPAT_KERNEL_RELEASE)
 			, notify_channel
 			#endif
-			, cur_network->network.cfg_80211_acl_data.mac_address
+			, cur_network->cfg_80211_acl_data.mac_address[0]
 			, pmlmepriv->assoc_req+sizeof(struct rtw_ieee80211_hdr_3addr)+2
 			, pmlmepriv->assoc_req_len-sizeof(struct rtw_ieee80211_hdr_3addr)-2
 			, pmlmepriv->assoc_rsp+sizeof(struct rtw_ieee80211_hdr_3addr)+6
