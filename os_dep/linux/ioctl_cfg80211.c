b2d809fb6b7a378df190854d657a1a311a1d8659
shangril <shangril@users.noreply.github.com>
2022-09-27 11:28:20 +0200

    Update ioctl_cfg80211.c

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -350,7 +350,7 @@ static u64 rtw_get_systime_us(void)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,39))
 	struct timespec64 *ts;
 	//get_monotonic_boottime(&ts);
-	ts = malloc(sizeof *ts); 
+	ts = NULL; 
 	ktime_get_boottime_ts64(ts);
 	return ((u64)ts->tv_sec*1000000) + ts->tv_nsec / 1000;
 #else
